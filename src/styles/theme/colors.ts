e705033900a851ee7af515c0d159a15e711b98fd
Taewoong Heo <chamjoeun0111@gmail.com>
2025-08-30 17:11:40 +0900

    chore(colors): red, text-default 컬러 추가

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -13,6 +13,19 @@ export const colors = {
     1000: '#1a1c20',
   },
 
+  red: {
+    0: '#fff6f7',
+    100: '#fee6e8',
+    200: '#fdd3d6',
+    300: '#fcbfc3',
+    400: '#fbabaf',
+    500: '#f9888c',
+    600: '#f8646a',
+    700: '#f64147',
+    800: '#f51d25',
+    900: '#f40000',
+  },
+
   brand: {
     0: '#F1E8DA',
     100: '#E3D1B5',
@@ -44,5 +57,6 @@ export const semanticColors = {
 
   text: {
     default: '#1F1F1F',
+    disabled: '#474747',
   },
 } as const;
