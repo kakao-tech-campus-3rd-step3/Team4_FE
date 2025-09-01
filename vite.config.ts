a97d09f0e2dc54c3a373ffced25dbf48c162f0f6
Taewoong Heo <chamjoeun0111@gmail.com>
2025-09-01 20:48:56 +0900

    fix(config): base 레포 이름 "-"을 "_" 으로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Team4-FE/',
+  base: '/Team4_FE/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
