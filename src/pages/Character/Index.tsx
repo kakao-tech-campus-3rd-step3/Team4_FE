e3e495068572a8deecb877eb8587fa6ec8edf0d6
Taewoong Heo <chamjoeun0111@gmail.com>
2025-09-05 14:10:25 +0900

    feat: 꼬리 이미지 추가

--- src/pages/Character/Index.tsx
+++ src/pages/Character/Index.tsx
@@ -1,3 +1,4 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const ImageContainer = styled.div`
@@ -5,12 +6,14 @@ const ImageContainer = styled.div`
   height: 80vh;
   overflow: hidden;
 
+  background-color: #000;
+
   position: relative;
 `;
 
 const BackgroundImage = styled.img`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   object-fit: cover;
 `;
 
@@ -20,9 +23,33 @@ const CharacterImage = styled.img`
   object-fit: cover;
   position: absolute;
 
-  bottom: 13%;
+  bottom: 22%;
   left: 50%;
   transform: translateX(-50%);
+`;
+
+const wagTail = keyframes`
+  from {
+    transform: translateX(-50%) rotate(0deg);
+  }
+  to {
+    transform: translateX(-50%) rotate(10deg);
+  }
+`;
+
+const TailImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  position: absolute;
+
+  bottom: 16.6%;
+  left: 62%;
+  transform: translateX(-50%);
+
+  background-color: #000;
+
+  animation: ${wagTail} 1s linear infinite alternate;
 `;
 
 function Index() {
@@ -32,14 +59,14 @@ function Index() {
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
       }}
     >
       <ImageContainer>
         <CharacterImage
           alt="character"
-          src={`${import.meta.env.BASE_URL}assets/character/default.png`}
+          src={`${import.meta.env.BASE_URL}assets/character/cat-no-tail.png`}
         />
+        <TailImage alt="tail" src={`${import.meta.env.BASE_URL}assets/character/tail.png`} />
         <BackgroundImage
           alt="bg"
           src={`${import.meta.env.BASE_URL}assets/character/background.png`}
