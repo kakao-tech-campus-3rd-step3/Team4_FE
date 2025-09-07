256a2e5808cd941c3c0808541b081be74459de3b
Taewoong Heo <chamjoeun0111@gmail.com>
2025-09-07 13:58:52 +0900

    feat(character): 상점 UI 구현

--- src/pages/Character/Character/Character.tsx
+++ src/pages/Character/Character/Character.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+import { Typography } from '../../../components/common/Typography';
+import mocks from '../../../mockSetup';
+import theme from '../../../styles/theme';
 import {
   BackgroundImage,
   CharacterContainer,
@@ -6,13 +10,31 @@ import {
   TailImage,
 } from './Character.styles';
 
+type Item = {
+  id: number;
+  category: 'HAT';
+  name: string;
+  price: number;
+  imageUrl: string;
+  isOwned: false;
+};
+
 function Index() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  console.log(items);
+
+  useEffect(() => {
+    setItems(mocks.characterStoreItemsMock);
+  }, []);
+
   return (
     <div
       style={{
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
+        marginBottom: 30,
       }}
     >
       <ImageContainer>
@@ -28,7 +50,92 @@ function Index() {
           src={`${import.meta.env.BASE_URL}assets/character/background.png`}
         />
       </ImageContainer>
-      <div></div>
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'row',
+            backgroundColor: 'rgb(221, 186, 136)',
+          }}
+        >
+          <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              borderTopRightRadius: 10,
+              borderTopLeftRadius: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: theme.colors.brand.background,
+              padding: 10,
+            }}
+          >
+            <Typography variant="body2Regular" color="default">
+              상점
+            </Typography>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              borderTopRightRadius: 10,
+              borderTopLeftRadius: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: theme.colors.brand.background,
+              padding: 10,
+            }}
+          >
+            <Typography variant="body2Regular" color="default">
+              보유중
+            </Typography>
+          </div>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            display: 'grid',
+            flexDirection: 'row',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gap: 10,
+            paddingInline: 10,
+          }}
+        >
+          {items &&
+            items.map((item) => (
+              <div
+                key={item.id}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: theme.colors.brand.primary,
+                  borderRadius: 10,
+                  padding: 10,
+                }}
+              >
+                <img
+                  style={{ height: 70, objectFit: 'contain' }}
+                  src={item.imageUrl}
+                  alt={item.name}
+                />
+                <Typography variant="body2Regular" color="default">
+                  {item.price}
+                </Typography>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
