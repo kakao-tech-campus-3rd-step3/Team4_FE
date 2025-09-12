40beafc6baf713429325f9c13ce068ad519e05fe
suhyeon <ksuhy0507@gmail.com>
2025-09-12 16:03:24 +0900

    refactor: missions mock 데이터 분리

---
+++ mocks/data/missionsMock.ts
@@ -0,0 +1,7 @@
+type Mission = { id: string; text: string };
+
+export const missionsMock: Mission[] = [
+  { id: 'm1', text: '백엔드 직군 채용정보 5개 찾아보기' },
+  { id: 'm2', text: '자기소개서에서 나의 강점 3가지 정리해보기' },
+  { id: 'm3', text: '1분 자기소개 발표해보기' },
+];
