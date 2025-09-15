6498cd3f6b14e0279283f84941e3878a1728858d
Taewoong Heo <chamjoeun0111@gmail.com>
2025-09-15 14:12:07 +0900

    feat(Character): 상점 보유 아이템 api 추가
    - 원래 탭 전환 시 effect 를 호출했는데 그게 불필요한 effect 라고 판단되어 setTab 에서 mocks 를 바꾸는 방식이었음
    - 그런데 state 초기화 시 데이터를 페칭하면, state 의 타입이 Promise 가 되어야 하는 문제가 있었음
    - 그래서 useEffect 를 다시 추가, 그리고 setTab 에 있던 데이터 핸들링 책임을 분리함

--- src/pages/Character/Character/Character.tsx
+++ src/pages/Character/Character/Character.tsx
@@ -1,7 +1,7 @@
 import { BASE_URL } from '@/constants/routes';
-import mocks from '@/mockSetup';
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import mocks from '../../../mockSetup';
 import { TABS } from '../constants/tab';
 import type { OwnedItem, SelectedItem, StoreItem } from '../types/Item';
 import type { Tab } from '../types/tab';
@@ -29,26 +29,47 @@ const SelectedItemImage = styled.img<{ x: number; y: number }>`
 
 function Character() {
   const [tab, setTab] = useState<Tab>(TABS.STORE);
-  const [storeItems, setStoreItems] = useState<StoreItem[]>(() => {
-    const storeItemsMock = mocks.data.characterStoreItemsMock;
-    return [...storeItemsMock];
-  });
+  const [storeItems, setStoreItems] = useState<StoreItem[]>([]);
   const [ownedItems, setOwnedItems] = useState<OwnedItem[]>([]);
-
   const [selectedItem, setSelectedItem] = useState<SelectedItem | null>(null);
 
-  const handleChangeTab = (nextTab: Tab) => {
-    setTab(nextTab);
+  const fetchStoreItems = async () => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/api/items?page=1&category=HAT`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+          },
+        },
+      );
 
-    if (nextTab === TABS.OWNED) {
+      // fetch 는 4xx, 5xx 에러를 reject 하지 않음
+      if (!res.ok) {
+        throw new Error(`error:  ${res.status}`);
+      }
+
+      const json = (await res.json()) as { content: StoreItem[] };
+      setStoreItems(json.content);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (tab === TABS.OWNED) {
       const ownedItemsMock = mocks.data.isOwnedItemsMock;
       setOwnedItems([...ownedItemsMock]);
       return;
     }
 
-    // 상점 아이템 목록 조회, GET, /api/items?page=Integer&category=ItemCategoryEnum
-    const storeItemsMock = mocks.data.characterStoreItemsMock;
-    setStoreItems([...storeItemsMock]);
+    fetchStoreItems();
+  }, [tab]);
+
+  const handleChangeTab = (nextTab: Tab) => {
+    setTab(nextTab);
   };
 
   return (
