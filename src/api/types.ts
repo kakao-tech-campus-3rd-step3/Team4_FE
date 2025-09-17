97d1a439d5311d22fa87497194099694672aeb96
suhyeon <ksuhy0507@gmail.com>
2025-09-17 22:07:09 +0900

    feat: api type.ts 설정

---
+++ src/api/types.ts
@@ -0,0 +1,44 @@
+export type CategoryEnum = 'REFRESH' | 'EMPLOYMENT' | 'DAILY';
+export type EmotionEnum = 'EXCELLENT' | 'GOOD' | 'SOSO' | 'BAD' | 'TERRIBLE';
+export type ItemCategoryEnum = 'HAT';
+
+export type Mission = {
+  id: number;
+  content: string;
+  category: CategoryEnum;
+};
+
+export type Diary = {
+  emotion: EmotionEnum;
+  content: string;
+  feedback: string;
+  createdAt: Date;
+};
+
+export type StoreItem = {
+  id: string;
+  category: ItemCategoryEnum;
+  name: string;
+  price: number;
+  imageUrl: string;
+  isOwned: boolean;
+};
+
+export type OwnedItem = {
+  id: string;
+  category: ItemCategoryEnum;
+  name: string;
+  imageUrl: string;
+  offsetX: number;
+  offsetY: number;
+  isUsed: Boolean;
+};
+
+export type Cat = {
+  name: string;
+  equipped: {
+    imageUrl: string;
+    offsetX: number;
+    offsetY: number;
+  };
+};
