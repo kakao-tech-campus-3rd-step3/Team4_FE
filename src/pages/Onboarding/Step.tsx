963ab5f9506547929158abbdddd83afe4a0ddaa6
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-12 13:22:58 +0900

    fix(intro): 레이아웃 오류 수정

--- src/pages/Onboarding/Step.tsx
+++ src/pages/Onboarding/Step.tsx
@@ -1,5 +1,95 @@
+import { Typography } from '@/components/common/Typography';
+import { ROUTES } from '@/constants/routes';
+import { semanticColors } from '@/styles/theme/colors';
+import type { ReactNode } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Container, NextButton } from './Test/Test.styles';
+
 function OnboardingStep() {
-  return <div>onboarding step</div>;
+  const router = useNavigate();
+  const { step } = useParams();
+
+  if (!step) {
+    alert('오류 발생! 다시 시도해주세요');
+    return router(ROUTES.HOME);
+  }
+
+  return <OnboardingStepRouter step={step} />;
+}
+
+function OnboardingStepRouter({ step }: { step: string }) {
+  const router = useNavigate();
+
+  if (step === ROUTES.ONBOARDING_STEP_RESULT) {
+    return (
+      <OnboardingStepLayout
+        onNext={() => router(`${ROUTES.ONBOARDING}/${ROUTES.ONBOARDING_STEP_INTRO}`)}
+        buttonText="다음"
+      >
+        <div>result</div>
+      </OnboardingStepLayout>
+    );
+  }
+
+  if (step === ROUTES.ONBOARDING_STEP_INTRO) {
+    return (
+      <OnboardingStepLayout
+        onNext={() => router(`${ROUTES.ONBOARDING}/${ROUTES.ONBOARDING_STEP_NAME}`)}
+        buttonText="다음"
+      >
+        <div>intro</div>
+      </OnboardingStepLayout>
+    );
+  }
+
+  if (step === ROUTES.ONBOARDING_STEP_NAME) {
+    return (
+      <OnboardingStepLayout
+        onNext={() => router(`${ROUTES.ONBOARDING}/${ROUTES.ONBOARDING_STEP_START}`)}
+        buttonText="다음"
+      >
+        <div>name</div>
+      </OnboardingStepLayout>
+    );
+  }
+
+  if (step === ROUTES.ONBOARDING_STEP_START) {
+    return (
+      <OnboardingStepLayout onNext={() => router(ROUTES.HOME)} buttonText="시작하기">
+        <div>start</div>
+      </OnboardingStepLayout>
+    );
+  }
+
+  return <div></div>;
+}
+
+type OnboardingStepLayoutProps = {
+  children: ReactNode;
+  onNext: () => void;
+  buttonText?: string;
+  buttonDisabled?: boolean;
+};
+
+function OnboardingStepLayout({
+  children,
+  onNext,
+  buttonText = '다음',
+  buttonDisabled = false,
+}: OnboardingStepLayoutProps) {
+  return (
+    <Container>
+      {/* 콘텐츠 영역 */}
+      <div>{children}</div>
+
+      {/* 하단 고정 버튼 */}
+      <NextButton onClick={onNext} disabled={buttonDisabled}>
+        <Typography variant="label2Regular" style={{ color: semanticColors.background.default }}>
+          {buttonText}
+        </Typography>
+      </NextButton>
+    </Container>
+  );
 }
 
 export default OnboardingStep;
