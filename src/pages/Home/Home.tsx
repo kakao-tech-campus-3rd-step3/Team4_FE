f7ea2e0855a2d5cded0c48c441a8e3197d784469
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-18 13:21:05 +0900

    feat: main 페이지 UI 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,63 @@
 import { Typography } from '@/components/common/Typography';
+import {
+  HomeContainer,
+  GreetingSection,
+  CharacterSection,
+  CharacterImage,
+  BackgroundImage,
+  ActionsSection,
+  MissionCard,
+  MissionItem,
+  MissionIcon,
+  DiaryCard,
+} from '@/components/Home/Home.styles';
+import { BASE_URL } from '@/constants/routes';
 
 function Home() {
   return (
-    <div>
-      <Typography variant="title1Bold" color="brown500">
-        Home
-      </Typography>
-    </div>
+    <HomeContainer>
+      <GreetingSection>
+        <Typography variant="title1Regular" color="gray900">
+          안녕하세요!
+        </Typography>
+        <Typography variant="title1Regular" color="gray900">
+          오늘 기분은 어떠신가요?
+        </Typography>
+      </GreetingSection>
+      <CharacterSection>
+        <CharacterImage src={`${BASE_URL}assets/character/happy1.png`} alt="character" />
+        <BackgroundImage src={`${BASE_URL}assets/character/background.png`} alt="background" />
+      </CharacterSection>
+      <ActionsSection>
+        <div>
+          <MissionCard>
+            <Typography variant="body2Regular" color="gray900">
+              오늘의 미션
+            </Typography>
+            <MissionItem>
+              <MissionIcon />
+              <Typography variant="label2Regular" color="gray800">
+                하늘을 바라보며 스트레칭하기
+              </Typography>
+            </MissionItem>
+            <MissionItem>
+              <MissionIcon />
+              <Typography variant="label2Regular" color="gray800">
+                산책하기
+              </Typography>
+            </MissionItem>
+          </MissionCard>
+        </div>
+        <DiaryCard>
+          <Typography variant="body2Regular" color="gray900">
+            일기쓰기
+          </Typography>
+          <Typography variant="label2Regular" color="gray800">
+            오늘 하루의 감정을 기록해보세요
+          </Typography>
+        </DiaryCard>
+      </ActionsSection>
+    </HomeContainer>
   );
 }
 
