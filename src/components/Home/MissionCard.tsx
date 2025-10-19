9a558e4ed1e6ce298c25fb837471a464f2ab5789
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-19 14:57:12 +0900

    feat(home): missions api 연결

--- src/components/Home/MissionCard.tsx
+++ src/components/Home/MissionCard.tsx
@@ -1,28 +1,33 @@
 import { Typography } from '@/components/common/Typography';
 import { MissionCard as StyledMissionCard, MissionItem, MissionIcon } from './Home.styles';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { MissionsAPI } from '@/api/missions';
+import type { Mission } from '@/api/types';
+import QUERY_KEY from '@/constants/queryKey';
 
 interface MissionCardProps {
   onClick: () => void;
 }
 
 export function MissionCard({ onClick }: MissionCardProps) {
+  const { data: dailyMissions } = useSuspenseQuery<Mission[]>({
+    queryKey: [QUERY_KEY.MISSIONS],
+    queryFn: MissionsAPI.getDailyMissions,
+  });
+
   return (
     <StyledMissionCard onClick={onClick}>
       <Typography variant="body2Regular" color="gray900">
         오늘의 미션
       </Typography>
-      <MissionItem>
-        <MissionIcon />
-        <Typography variant="label2Regular" color="gray800">
-          하늘을 바라보며 스트레칭하기
-        </Typography>
-      </MissionItem>
-      <MissionItem>
-        <MissionIcon />
-        <Typography variant="label2Regular" color="gray800">
-          산책하기
-        </Typography>
-      </MissionItem>
+      {dailyMissions.map((mission) => (
+        <MissionItem key={mission.id}>
+          <MissionIcon />
+          <Typography variant="label2Regular" color="gray800">
+            {mission.content}
+          </Typography>
+        </MissionItem>
+      ))}
     </StyledMissionCard>
   );
 }
