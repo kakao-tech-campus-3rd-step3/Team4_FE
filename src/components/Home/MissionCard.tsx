af5a347678dd2d6d65968c93c098555e4d89fb65
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-20 13:55:16 +0900

    refactor: 일일 미션 api 타입 변경

--- src/components/Home/MissionCard.tsx
+++ src/components/Home/MissionCard.tsx
@@ -2,7 +2,7 @@ import { Typography } from '@/components/common/Typography';
 import { MissionCard as StyledMissionCard, MissionItem, MissionIcon } from './Home.styles';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { MissionsAPI } from '@/api/missions';
-import type { Mission } from '@/api/types';
+import type { Plan } from '@/api/types';
 import QUERY_KEY from '@/constants/queryKey';
 
 interface MissionCardProps {
@@ -10,7 +10,7 @@ interface MissionCardProps {
 }
 
 export function MissionCard({ onClick }: MissionCardProps) {
-  const { data: dailyMissions } = useSuspenseQuery<Mission[]>({
+  const { data: dailyMissions } = useSuspenseQuery<Plan[]>({
     queryKey: [QUERY_KEY.MISSIONS],
     queryFn: MissionsAPI.getDailyMissions,
   });
@@ -21,15 +21,15 @@ export function MissionCard({ onClick }: MissionCardProps) {
         오늘의 미션
       </Typography>
       {dailyMissions.length === 0 ? (
-        <Typography variant="label2Regular" color="gray600">
+        <Typography variant="label2Regular" color="gray800">
           오늘은 미션이 없습니다.
         </Typography>
       ) : (
-        dailyMissions.map((mission) => (
-          <MissionItem key={mission.id}>
+        dailyMissions.map((plan) => (
+          <MissionItem key={plan.id}>
             <MissionIcon />
             <Typography variant="label2Regular" color="gray800">
-              {mission.content}
+              {plan.content}
             </Typography>
           </MissionItem>
         ))
