6d253c389c718ad9f60bea747c6e930a86f55dc0
Taewoong Heo <chamjoeun0111@gmail.com>
2025-10-27 22:03:43 +0900

    fix(home): 미션카드 클릭 시 라우터 문제 수정

--- src/components/Home/MissionCard.tsx
+++ src/components/Home/MissionCard.tsx
@@ -1,10 +1,18 @@
 import { Typography } from '@/components/common/Typography';
 import { MissionCard as StyledMissionCard } from './Home.styles';
 import { MissionCardWithSuspense } from './MissionCardWithSuspense';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/constants/routes';
 
 function MissionCard() {
+  const router = useNavigate();
+
+  const onClick = () => {
+    router(ROUTES.MISSIONS);
+  };
+
   return (
-    <StyledMissionCard>
+    <StyledMissionCard onClick={onClick}>
       <Typography variant="body2Regular" color="gray900">
         오늘의 미션
       </Typography>
